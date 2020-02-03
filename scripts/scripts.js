f4cbb50eafcc6ddba581997baea76cd41c9d6b4a
Marcin Gorecki <marcingorecki1981@gmail.com>
2020-02-04 00:04:08 +0100

    Bootstrap after all + slider and top nav

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,21 @@
-window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-}); 
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  scroll_pos < 5 ?
+    document.querySelector('nav').classList.remove('transparent') :
+    document.querySelector('nav').classList.add('transparent')
+}
+
+window.addEventListener('scroll', function (e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function () {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
