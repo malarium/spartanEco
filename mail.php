565b2e9ed6d5c5660ee04c0eb32b4b6164992b13
Marcin <marcingorecki1981@gmail.com>
2020-02-20 20:08:51 +0100

    map and mail

--- mail.php
+++ mail.php
@@ -5,20 +5,21 @@
 </head>
 
 <?php
-  $do = "biuro@webdogs.pl";
+  $do = "malarium@wp.pl";
   if(isset($_POST['temat']) && isset($_POST['wiadomosc']) && isset($_POST['mail_zw'])) {
     $temat = $_POST['temat'];
     $tresc = $_POST['wiadomosc'];
     $mail = $_POST['mail_zw'];
     if($temat=="" || $tresc=="" || $mail=="") {
       echo "<h1><center>Uzupełnij wszystkie pola!</center></h1>";
-      echo "<meta http-equiv='Refresh' content='2;url=kontakt' />";
+      echo "<meta http-equiv='Refresh' content='2;url=index.html' />";
     }
     else {
       $headers = "From: ".$mail."\r\n";
       $headers .= "Content-Type: text/plain; charset=UTF-8\n";
       $headers .= "Content-Transfer-Encoding: 8bit\n";
       if(mail($do, $temat, $tresc, $headers)){
+        mail($do, $temat, $tresc, $headers);
         echo '<h1><center>Wiadomość wysłana, dziękujemy!</center></h1>';
       }
       else{
