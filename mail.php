f718dc909cbfada93ad78dd7941d8672e80e16e9
Marcin <marcingorecki1981@gmail.com>
2020-02-21 10:55:49 +0100

    POST to GET trial

--- mail.php
+++ mail.php
@@ -6,18 +6,18 @@
 
 <?php
   $do = "malarium@wp.pl";
-  if(isset($_POST['temat']) && isset($_POST['wiadomosc']) && isset($_POST['mail_zw'])) {
-    $temat = $_POST['temat'];
-    $tresc = $_POST['wiadomosc'];
-    $mail = $_POST['mail_zw'];
+  if(isset($_GET['temat']) && isset($_GET['wiadomosc']) && isset($_GET['mail_zw'])) {
+    $temat = $_GET['temat'];
+    $tresc = $GET['wiadomosc'];
+    $mail = $_GET['mail_zw'];
     if($temat=="" || $tresc=="" || $mail=="") {
       echo "<h1><center>Uzupełnij wszystkie pola!</center></h1>";
       echo "<meta http-equiv='Refresh' content='2;url=index.html' />";
     }
     else {
       $headers = "From: ".$mail."\n";
-      $headers .= "Content-Type: text/plain; charset=UTF-8\n";
-      $headers .= "Content-Transfer-Encoding: 8bit\n";
+      //$headers .= "Content-Type: text/plain; charset=UTF-8\n";
+      //$headers .= "Content-Transfer-Encoding: 8bit\n";
       if(mail($do, $temat, $tresc, $headers)){
         echo '<h1><center>Wiadomość wysłana, dziękujemy!</center></h1>';
       }
